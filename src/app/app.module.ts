a0c77589a038177f07d70599b176f576a2904e42
ThanhLong34 <thanhlongedu0304@gmail.com>
2024-09-13 20:28:44 +0700

    Update UI login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './demo/shared/shared.module';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/demo/pages/auth/login/login.component.ts
+++ src/app/demo/pages/auth/login/login.component.ts
@@ -1,33 +1,23 @@
 // angular import
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormControl, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 // project import
 import { SharedModule } from 'src/app/demo/shared/shared.module';
 
+// primeNG import
+import { MessageService } from 'primeng/api';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SharedModule, RouterModule],
+  imports: [SharedModule, RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss', '../authentication.scss']
 })
 export default class LoginComponent {
-  // public props
-  hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
-  Email = 'info@phoenixcoded.co';
-  password = '123456';
-
-  // public method
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter an email';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
   loginType = [
     {
       image: 'assets/images/authentication/facebook.svg',
@@ -45,4 +35,36 @@ export default class LoginComponent {
       title: 'Sign In with Google'
     }
   ];
+  formData!: FormGroup;
+
+  constructor(private messageService: MessageService) {
+    this.formData = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      remember: new FormControl(false)
+    });
+  }
+
+  get emailIsInvalid() {
+    const emailCtrl = this.formData.controls['email'];
+    return emailCtrl.touched && emailCtrl.dirty && emailCtrl.invalid;
+  }
+
+  get passwordIsInvalid() {
+    const passwordCtrl = this.formData.controls['password'];
+    return passwordCtrl.touched && passwordCtrl.dirty && passwordCtrl.invalid;
+  }
+
+  handleSubmit() {
+
+  }
+
+  handleShowToast(inputType: string) {
+    if (inputType === 'email' && this.emailIsInvalid) {
+      this.messageService.add({ severity: 'warn', summary: 'Warning', detail: 'Email is invalid' });
+    }
+    if (inputType === 'password' && this.passwordIsInvalid) {
+      this.messageService.add({ severity: 'warn', summary: 'Warning', detail: 'Password is invalid' });
+    }
+  }
 }
